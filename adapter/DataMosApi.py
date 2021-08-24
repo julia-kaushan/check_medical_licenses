393df85ecd3bcd443274e03ed059cb541e488120
Julia Kaushan <julia-kaushan@mail.ru>
2021-08-24 18:11:20 +0300

    Исправлена функция get

--- adapter/DataMosApi.py
+++ adapter/DataMosApi.py
@@ -24,19 +24,17 @@ class DataMosApi:
             if response.status_code != 200:
                 raise requests.exceptions.ConnectionError
             res = response.json()
+            org.name = res['Result'][0]['Cells']['LicenseHolderName']
+            org.inn = res['Result'][0]['Cells']['INN']
+            org.adress = res['Result'][0]['Cells']['Address']
             count = res['Count']
             # мы не можем определить, является ли лицензия действующей
             if count > 10:
                 count = 10
-            org.name = res['Result'][0]['Cells']['LicenseHolderName']
-            org.inn = res['Result'][0]['Cells']['INN']
-            org.adress = res['Result'][0]['Cells']['Address']
             for i in range(count):
-                org.licenses.append({'id': (i + 1),
-                                     'Номер лицензии': res['Result'][i]['Cells']['License'],
-                                     'Дата выдачи лицензии': res['Result'][i]['Cells']['RegistrationDate'],
-                                     'Вид деятельности': res['Result'][i]['Cells']['PermissionList']
-                                     })
+                org.licenses.append({'id': i+1,
+                                'Номер лицензии': res['Result'][i]['Cells']['License'],
+                                'Дата выдачи лицензии': res['Result'][i]['Cells']['RegistrationDate']})
             return org
         except requests.exceptions.ConnectionError as err:
             print('Сервер не доступен', err)
