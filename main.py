8cf6eebee4b145e78cade2b866bdbbce39af364c
Julia Kaushan <julia-kaushan@mail.ru>
2021-08-24 18:12:21 +0300

    Добавлено чтение и запись результатов в exel file

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 from adapter.DataMosApi import DataMosApi
+import pandas as pd
+
 
 a = DataMosApi()
 
-list_ogrn = [1077757418111, 1027739726233, 1097746129843, 1027739542962, 1027700345012, 1117746464296, 1077761370169, 1057746089169]
+
+ogrn = pd.read_excel('./ogrn.xlsx', index_col=None)
+list_ogrn = ogrn["ОГРН"].values.tolist()
+
+df = pd.DataFrame({'Название': [], 'ОГРН': [], 'ИНН': [], 'Адрес': [], 'Лицензия': []})
 
 b = a.get_all(list_ogrn)
+
+name_list, inn_list, adress_list, licenses_list, = list(), list(), list(), list()
+
 for i in range(len(list_ogrn)):
-    print(b[i].name)
+    name_list.append(b[i].name)
+    inn_list.append(b[i].inn)
+    adress_list.append(b[i].adress)
+    licenses_list.append(b[i].licenses)
+
+for i in range(len(list_ogrn)):
+    df['Название'] = name_list
+    df['ОГРН'] = list_ogrn
+    df['ИНН'] = inn_list
+    df['Адрес'] = adress_list
+    df['Лицензия'] = licenses_list
 
 
+print(df.head())
 
-
+df.to_excel('./result.xlsx')
