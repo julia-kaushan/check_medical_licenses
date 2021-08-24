4f22c19e4832549665f082257a41afbafa9da5b7
Julia Kaushan <julia-kaushan@mail.ru>
2021-08-24 18:09:07 +0300

    Исправлен параметр "лицензии" в конструкторе

--- models/MedicalOrg.py
+++ models/MedicalOrg.py
@@ -2,9 +2,10 @@ class MedicalOrg:
 
     def __init__(self, ogrn):
         self.ogrn = ogrn
+        self.licenses = list()
+
 
     name = ''
     inn = None
     ogrn = None
     adress = ''
-    licenses = []
