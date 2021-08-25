256dce3b746acc47c59dc3ec635f826417092efd
Julia Kaushan <julia-kaushan@mail.ru>
2021-08-25 14:24:56 +0300

    Создан класс для чтения и формирования excel файла

---
+++ excel/Excel.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+from models import MedicalOrg
+
+
+class Excel:
+    def __init__(self):
+        self.df = pd.DataFrame({'Название': [], 'ОГРН': [], 'ИНН': [], 'Адрес': [], 'Лицензия': []})
+
+
+    def read(self, name_file, column_name):
+        ogrn = pd.read_excel(name_file)
+        list_ogrn = ogrn[column_name].values.tolist()
+        return list_ogrn
+
+    def build(self, name_file: str, list_org):
+        org = list_org
+        for org in list(map(lambda org: {"Название": org.name,
+                                         'ОГРН': org.ogrn,
+                                         'ИНН': org.inn,
+                                         'Адрес': org.adress,
+                                         'Лицензия': org.licenses}, list_org)):
+            self.df = self.df.append(org, ignore_index=True)
+        print(self.df)
+        self.df.to_excel(name_file)
+
+# ogrn = pd.read_excel('./ogrn.xlsx', index_col=None)
