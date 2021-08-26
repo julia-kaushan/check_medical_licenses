1350fe246f8dd041de796f34faf826ac2fa5cd68
Julia Kaushan <julia-kaushan@mail.ru>
2021-08-26 17:31:19 +0300

    Формирование excel из бинарных данных

--- excel/Excel.py
+++ excel/Excel.py
@@ -1,18 +1,21 @@
 import pandas as pd
+from io import BytesIO
 
 
 class Excel:
-    def __init__(self):
+    def __init__(self, excel_binary):
         self.df = pd.DataFrame({'Название': [], 'ОГРН': [], 'ИНН': [], 'Адрес': [], 'Лицензия': []})
+        self.excel_binary = excel_binary
 
 
-    def read(self, name_file, column_name):
-        ogrn = pd.read_excel(name_file)
+
+    def read(self, column_name):
+        ogrn = pd.read_excel(self.excel_binary)
         list_ogrn = ogrn[column_name].values.tolist()
         return list_ogrn
 
 
-    def build(self, name_file: str, list_org):
+    def build(self, list_org):
         org = list_org
         for org in list(map(lambda org: {"Название": org.name,
                                          'ОГРН': org.ogrn,
@@ -20,6 +23,11 @@ class Excel:
                                          'Адрес': org.adress,
                                          'Лицензия': org.licenses}, list_org)):
             self.df = self.df.append(org, ignore_index=True)
-        print(self.df)
-        self.df.to_excel(name_file)
+        bio = BytesIO()
+        writer = pd.ExcelWriter(bio, engine="xlsxwriter")
+        self.df.to_excel(writer, sheet_name="Sheet1")
+        writer.save()
+        bio.seek(0)
+        return bio
+
 
