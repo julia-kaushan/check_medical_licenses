e0cd27bc194b310e3e1f9e6fbc62c846048a6d8b
Julia Kaushan <julia-kaushan@mail.ru>
2021-08-26 17:32:05 +0300

    Перенесение обработчика ошибок в app.py

--- adapter/DataMosApi.py
+++ adapter/DataMosApi.py
@@ -19,25 +19,22 @@ class DataMosApi:
     def get(self, ogrn):
         org = MedicalOrg(ogrn)
         self.params['filters[0].Value'] = ogrn
-        try:
-            response = requests.get(self.API, params=self.params)
-            if response.status_code != 200:
-                raise requests.exceptions.ConnectionError
-            res = response.json()
-            org.name = res['Result'][0]['Cells']['LicenseHolderName']
-            org.inn = res['Result'][0]['Cells']['INN']
-            org.adress = res['Result'][0]['Cells']['Address']
-            count = res['Count']
-            # мы не можем определить, является ли лицензия действующей
-            if count > 10:
-                count = 10
-            for i in range(count):
-                org.licenses.append({'id': i+1,
-                                'Номер лицензии': res['Result'][i]['Cells']['License'],
-                                'Дата выдачи лицензии': res['Result'][i]['Cells']['RegistrationDate']})
-            return org
-        except requests.exceptions.ConnectionError as err:
-            print('Сервер не доступен', err)
+        response = requests.get(self.API, params=self.params)
+        if response.status_code != 200:
+            raise requests.exceptions.ConnectionError
+        res = response.json()
+        org.name = res['Result'][0]['Cells']['LicenseHolderName']
+        org.inn = res['Result'][0]['Cells']['INN']
+        org.adress = res['Result'][0]['Cells']['Address']
+        count = res['Count']
+        # мы не можем определить, является ли лицензия действующей
+        if count > 10:
+            count = 10
+        for i in range(count):
+            org.licenses.append({'id': i+1,
+                            'Номер лицензии': res['Result'][i]['Cells']['License'],
+                            'Дата выдачи лицензии': res['Result'][i]['Cells']['RegistrationDate']})
+        return org
 
 
     def get_all(self, list_ogrn):
